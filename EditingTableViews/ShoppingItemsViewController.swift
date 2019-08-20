822e69182c669c792e686802cf422f129a77509e
Jason Ruan <jasonruan@pursuit.org>
2019-08-20 11:14:29 -0400

    Adds feature to put items into appropriate section and fixes editing/deleting features to accomodate new change

--- EditingTableViews/ShoppingItemsViewController.swift
+++ EditingTableViews/ShoppingItemsViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class ShoppingItemsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var shoppingItems: [ShoppingItem]!
+    var shoppingItems: [[ShoppingItem]] = [ShoppingItemFetchingClient.getShoppingItems(), []]
     
     @IBOutlet weak var shoppingTableView: UITableView!
     
@@ -14,16 +14,17 @@ class ShoppingItemsViewController: UIViewController, UITableViewDelegate, UITabl
             //maybe alert
             fatalError("Could not convert text from price text field")
         }
-        
+        //create new object of ShoppingItem based on user input and add to section 0
         let newItem = ShoppingItem(name: textFromName, price: priceAsDouble)
-        shoppingItems.append(newItem)
-        
+        shoppingItems[0].append(newItem)
+        //add new row to the relevant tableview section
         let lastIndex = shoppingTableView.numberOfRows(inSection: 0)
         let lastIndexPath = IndexPath(row: lastIndex, section: 0)
         shoppingTableView.insertRows(at: [lastIndexPath], with: .automatic)
     }
     
     @IBAction func editButton(_ sender: UIButton) {
+        //toggles the editing feature, and changes the text of the button as approriate
         if shoppingTableView.isEditing {
             shoppingTableView.setEditing(false, animated: true)
             sender.setTitle("Edit", for: UIControl.State.normal)
@@ -36,13 +37,7 @@ class ShoppingItemsViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         configureShoppingItemsTableView()
-        loadShoppingItems()
         shoppingTableView.setEditing(false, animated: false)
-    }
-    
-    private func loadShoppingItems() {
-        let allItems = ShoppingItemFetchingClient.getShoppingItems()
-        shoppingItems = allItems
     }
     
     private func configureShoppingItemsTableView() {
@@ -51,25 +46,32 @@ class ShoppingItemsViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Shopping Items"
+        switch section {
+        case 0:
+            return "Items Available For Purchase"
+        case 1:
+            return "Purchased Items"
+        default:
+            return ""
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shoppingItems.count
+        return shoppingItems[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //populates the cell with info from ShoppingItem
         let cell = tableView.dequeueReusableCell(withIdentifier: "items", for: indexPath)
-        cell.textLabel?.text = shoppingItems[indexPath.row].name
-        cell.detailTextLabel?.text = "$\(shoppingItems[indexPath.row].price)"
+        cell.textLabel?.text = shoppingItems[indexPath.section][indexPath.row].name
+        cell.detailTextLabel?.text = "$\(shoppingItems[indexPath.section][indexPath.row].price)"
         
         //alternates background color of consecutive cells
-        if indexPath.row % 2 == 0 {
+        if indexPath.section % 2 == 0 {
             cell.backgroundColor = UIColor(red: 1, green: 0.988, blue: 0.473, alpha: 1)
         } else {
             cell.backgroundColor = UIColor(red: 0.451, green: 0.988, blue: 0.838, alpha: 1)
@@ -81,24 +83,50 @@ class ShoppingItemsViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         switch editingStyle {
         case .delete:
-            shoppingItems.remove(at: indexPath.row)
-            shoppingTableView.deleteRows(at: [indexPath], with: .fade)
+            if indexPath.section == 0 {
+                //when user wants delete reference to item in its entirety
+                shoppingItems[0].remove(at: indexPath.row)
+                shoppingTableView.deleteRows(at: [indexPath], with: .fade)
+            } else {
+                //when user tries to delete an item from purchased section, add back to available section
+                shoppingItems[0].append(shoppingItems[1][indexPath.row])
+                let lastRow = shoppingTableView.numberOfRows(inSection: 0)
+                let lastIndexPath = IndexPath(row: lastRow, section: 0)
+                shoppingTableView.insertRows(at: [lastIndexPath], with: .automatic)
+                shoppingItems[1].remove(at: indexPath.row)
+                shoppingTableView.deleteRows(at: [indexPath], with: .fade)
+            }
         default:
             break
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let segueIdentifier = segue.identifier else { fatalError("No identifier in segue") }
-
-        switch segueIdentifier {
-        case "switchToAdd":
-            let _ = segue.destination as? AddItemViewController
-            let _ = shoppingTableView.indexPathForSelectedRow
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            //add item of tapped cell into other section's array and tableview section
+            shoppingItems[1].append(shoppingItems[0][indexPath.row])
+            let lastRow = shoppingTableView.numberOfRows(inSection: 1)
+            let lastIndexPath = IndexPath(row: lastRow, section: 1)
+            shoppingTableView.insertRows(at: [lastIndexPath], with: .automatic)
+            //remove item from original location in array and tableview
+            shoppingItems[0].remove(at: indexPath.row)
+            shoppingTableView.deleteRows(at: [indexPath], with: .fade)
+            
+        case 1:
+            //add item of tapped cell into other section's array and tableview section
+            shoppingItems[0].append(shoppingItems[1][indexPath.row])
+            let lastRow = shoppingTableView.numberOfRows(inSection: 0)
+            let lastIndexPath = IndexPath(row: lastRow, section: 0)
+            shoppingTableView.insertRows(at: [lastIndexPath], with: .automatic)
+            //remove item from original location in array and tableview
+            shoppingItems[1].remove(at: indexPath.row)
+            shoppingTableView.deleteRows(at: [indexPath], with: .fade)
+            
         default:
-            fatalError("Unexpected segue identifier")
+            break
         }
     }
-
+    
 }
 
